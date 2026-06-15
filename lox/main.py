682adaf7808ba724450b44cd8e3a35af64edcfcc
ZXPrism <150247305+ZXPrism@users.noreply.github.com>
2026-06-15 21:10:29 +0800

    basic argparse

--- lox/main.py
+++ lox/main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from lox!")
+import argparse
+
+
+class LoxEngine:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(
+            prog="lox",
+            description="Lox language interpreter",
+        )
+        self.parser.add_argument(
+            "script",
+            nargs="?",
+            help="Path to a Lox script to execute",
+        )
+
+    def main(self):
+        args = self.parser.parse_args()
+        if args.script:
+            self.run_file(args.script)
+        else:
+            self.run_repl()
+
+    def run_file(self, src_path: str):
+        print(src_path)
+
+    def run_repl(self):
+        print("Lox 0.1.0 - b260615")
+
+
+def entrypoint():
+    engine = LoxEngine()
+    engine.main()
 
 
 if __name__ == "__main__":
-    main()
+    entrypoint()
