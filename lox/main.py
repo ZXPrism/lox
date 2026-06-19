6cbb84c49dee911c289f46f45e2a9141ac7c4cda
ZXPrism <150247305+ZXPrism@users.noreply.github.com>
2026-06-19 21:28:41 +0800

    u

--- lox/main.py
+++ lox/main.py
@@ -1,5 +1,20 @@
 import argparse
 
+from dataclasses import dataclass
+
+
+@dataclass
+class Token:
+    pass
+
+
+class Scanner:
+    def __init__(self, src: str):
+        pass
+
+    def scan_tokens(self) -> list[Token]:
+        return []
+
 
 class LoxEngine:
     def __init__(self):
@@ -21,10 +36,28 @@ class LoxEngine:
             self.run_repl()
 
     def run_file(self, src_path: str):
-        print(src_path)
+        with open(src_path, "r") as fp:
+            self._run(fp.read())
 
     def run_repl(self):
         print("Lox 0.1.0 - b260615")
+
+        while True:
+            print(">", end=" ")
+            line = input()
+            self._run(line)
+
+    def _run(self, src: str):
+        scanner = Scanner(src)
+        tokens = scanner.scan_tokens()
+        for token in tokens:
+            print(token)
+
+    def _error(self, line_no: int, msg: str):
+        self.report(line_no, "", msg)
+
+    def _report(self, line_no: int, where: str, msg: str):
+        print()
 
 
 def entrypoint():
