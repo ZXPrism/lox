5d0094d4c9c435d71f82c709afc111637e2d119d
ZXPrism <150247305+ZXPrism@users.noreply.github.com>
2026-06-27 17:01:37 +0800

    ast types gen

--- lox/main.py
+++ lox/main.py
@@ -1,59 +1,8 @@
 import argparse
 
-from dataclasses import dataclass
-from enum import IntEnum
 from typing import Callable
-
-
-class TokenType(IntEnum):
-    # Single-character tokens.
-    LEFT_PAREN = 1  # (
-    RIGHT_PAREN = 2  # )
-    LEFT_BRACE = 3  # {
-    RIGHT_BRACE = 4  # }
-    COMMA = 5  # ,
-    DOT = 6  # .
-    MINUS = 7  # -
-    PLUS = 8  # +
-    SEMICOLON = 9  # ;
-    SLASH = 10  # /
-    STAR = 11  # *
-
-    # One or two character tokens.
-    BANG = 12  # !
-    BANG_EQUAL = 13  # !=
-    EQUAL = 14  # =
-    EQUAL_EQUAL = 15  # ==
-    GREATER = 16  # >
-    GREATER_EQUAL = 17  # >=
-    LESS = 18  # <
-    LESS_EQUAL = 19  # <=
-
-    # Literals.
-    IDENTIFIER = 20
-    STRING = 21
-    NUMBER = 22
-
-    # Keywords.
-    AND = 23
-    CLASS = 24
-    ELSE = 25
-    FALSE = 26
-    FUN = 27
-    FOR = 28
-    IF = 29
-    NIL = 30
-    OR = 31
-    PRINT = 32
-    RETURN = 33
-    SUPER = 34
-    THIS = 35
-    TRUE = 36
-    VAR = 37
-    WHILE = 38
-
-    EOF = 39
-
+from rich import print
+from lox.token import Token, TokenType
 
 _map_keyword_to_token_type = {
     "and": TokenType.AND,
@@ -73,17 +22,6 @@ _map_keyword_to_token_type = {
     "var": TokenType.VAR,
     "while": TokenType.WHILE,
 }
-
-
-@dataclass
-class Token:
-    type: TokenType
-    lexeme: str
-    literal: object | None
-    line_no: int
-
-    def to_string(self) -> str:
-        return f"{self.type.name} {self.lexeme} {self.literal}"
 
 
 class Scanner:
@@ -216,7 +154,7 @@ class Scanner:
         return self._source[self._current_pos + 1]
 
     def _scan_string_literal(self):
-        while self._peek() != '"' and not self._is_at_end():
+        while self._peek() != '"':
             if self._peek() == "\n":
                 self._line_no += 1
             self._advance()
---
+++ lox/scripts/gen_ast_types.py
@@ -0,0 +1,44 @@
+import argparse
+
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader
+
+
+def entrypoint(argv=None):
+    parser = argparse.ArgumentParser(
+        prog="gen_ast_types",
+        description="Lox AST types generator",
+    )
+    parser.add_argument("-o", "--output", required=True, help="output path")
+    args = parser.parse_args() if argv is None else parser.parse_args(argv)
+
+    env = Environment(loader=FileSystemLoader(Path(__file__).parent))
+    template = env.get_template("ast_types.py.jinja2")
+
+    src = template.render(
+        base_class_name="Expression",
+        types=[
+            _make_type(
+                "Binary", ["Expression left", "Token operator", "Expression right"]
+            ),
+            _make_type("Grouping", ["Expression expression"]),
+            _make_type("Literal", ["object value"]),
+            _make_type("Unary", ["Token operator", "Expression right"]),
+        ],
+    )
+
+    with open(args.output, "w") as fp:
+        fp.write(src)
+
+
+def _make_type(name: str, members: list[str]) -> dict:
+    fields = []
+    for mem in members:
+        type_name, field_name = mem.rsplit(" ", 1)
+        fields.append({"type": type_name, "name": field_name})
+    visit_method = name[0].lower() + name[1:]
+    return {"name": name, "fields": fields, "visit_method": visit_method}
+
+
+if __name__ == "__main__":
+    entrypoint()
